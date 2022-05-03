67132c62741e0b8fcd37ae9429d809bab004393a
masingerzero <masingerzero@gmail.com>
2022-05-03 12:17:31 +0200

    another commit

--- java-lambda/modern-java/src/main/java/org/masingerzero/modernjava/model/Transaction.java
+++ java-lambda/modern-java/src/main/java/org/masingerzero/modernjava/model/Transaction.java
@@ -1,6 +1,9 @@
 package org.masingerzero.modernjava.model;
 
+import java.util.List;
 import java.util.Objects;
+
+import static java.util.Arrays.asList;
 
 public class Transaction {
 
@@ -55,5 +58,23 @@ public class Transaction {
   public String toString() {
     return String.format("{%s, year: %d, value: %d}", trader, year, value);
   }
+
+  public static List<Transaction> transactions() {
+    Trader raoul = new Trader("Raoul", "Cambridge");
+    Trader mario = new Trader("Mario", "Milan");
+    Trader alan = new Trader("Alan", "Cambridge");
+    Trader brian = new Trader("Brian", "Cambridge");
+
+    List<Transaction> transactions = asList(
+            new Transaction(brian, 2011, 300),
+            new Transaction(raoul, 2012, 1000),
+            new Transaction(raoul, 2011, 400),
+            new Transaction(mario, 2012, 710),
+            new Transaction(mario, 2012, 700),
+            new Transaction(alan, 2012, 950)
+    );
+    return transactions;
+  }
+
 
 }
