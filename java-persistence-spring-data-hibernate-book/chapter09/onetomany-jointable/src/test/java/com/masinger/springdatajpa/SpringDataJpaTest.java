8f191b04795dc2e49f782d62dc3426cbbc2a5631
masingerzero <masingerzero@gmail.com>
2023-01-21 19:23:51 +0100

    commit

---
+++ java-persistence-spring-data-hibernate-book/chapter09/onetomany-jointable/src/test/java/com/masinger/springdatajpa/SpringDataJpaTest.java
@@ -0,0 +1,19 @@
+package com.masinger.springdatajpa;
+
+import com.masinger.springdatajpa.configuration.SpringDataConfiguration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringDataConfiguration.class})
+public class SpringDataJpaTest {
+
+    @Test
+    void storeLoadEntities() {
+
+
+    }
+}
--- modern-java/src/main/java/org/masingerzero/modernjava/chapter11/Chapter11SandBox.java
+++ modern-java/src/main/java/org/masingerzero/modernjava/chapter11/Chapter11SandBox.java
@@ -6,33 +6,28 @@ import java.util.Optional;
 
 public class Chapter11SandBox {
     public static void main(String[] args) {
+
+
         Person person = new Person();
-        String insuranceName = getInsuranceName(person);
-        System.out.println(insuranceName);
-
-
+        Optional<Person> optPerson = Optional.of(person);
+        Optional<String> s = optPerson.flatMap(Person::getCar)
+                .flatMap(Car::getInsurance)
+                .map(Insurance::getName);
     }
 
-    private static String getInsuranceName(Person person) {
-        String name = "NO-INSURANCE";
-        String name = person.getOptionalCar().orElse(new Car()).getOptionalInsurance().orElse(new Insurance()).getName();
-        person.getOptionalCar().orElse(new Car()).getOptionalInsurance().ifPresent();
-        String name1 =
-        return name;
 
-    }
 }
 
 @Data
 class Person {
-    private Car car;
-    private Optional<Car> optionalCar = Optional.empty();
+
+    private Optional<Car> car = Optional.empty();
 }
 
 @Data
 class Car {
-    private Insurance insurance;
-    private Optional<Insurance> optionalInsurance = Optional.empty();
+
+    private Optional<Insurance> insurance = Optional.empty();
 }
 
 @Data
